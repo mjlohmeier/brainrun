d266a625ed70fc5ac32e2ae1920d409a8791eb25
Michael Lohmeier <mjlohmeier@gmail.com>
2018-08-01 15:31:04 -0400

    made more descriptive variable names

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var orderForm = document.querySelector('[data-brains-order="form"]');
 
-var nameList = document.querySelector(".order-list");
+var nameListContainer = document.querySelector(".order-list");
 
-var emailList = document.querySelector(".order-list");
+var emailListContainer = document.querySelector(".order-list");
 
-var gooeynessList = document.querySelector(".order-list");
+var gooeynessListContainer = document.querySelector(".order-list");
 
 var orders = []
 
@@ -12,11 +12,11 @@ var submitAndReceiveBrains = function (event) {
     
     event.preventDefault();
 
-    var zombieNameInput = document.querySelector("[name=nameOfZombie]") 
+    var zombieNameInput = document.querySelector("[name=coffee]") 
 
-    var zombEmailInput = document.querySelector("[name=zombEmailAddress]")
+    var zombEmailInput = document.querySelector("[name=email]")
 
-    var gooeynessInput = document.querySelector("[name=levelOfGooeyness]")
+    var gooeynessInput = document.querySelector("[name=strength]")
 
     var zombieName = zombieNameInput.value
 
@@ -26,11 +26,17 @@ var submitAndReceiveBrains = function (event) {
 
     printOrder(zombieName, zombEmail, gooeyness)
 
-    orders.push({name:zombieName, email:zombEmail, gooeyness:gooeyness});
+    var orderForBrains = {coffee:zombieName, emailAddress:zombEmail, strength:gooeyness};
+
+    orders.push(orderForBrains)
 
     var brainStorage = JSON.stringify(orders)
 
     localStorage.setItem("orders", brainStorage)
+
+    $.ajax('https://dc-coffeerun.herokuapp.com/api/coffeeorders', {method: 'POST', data: orderForBrains
+})
+
 
 }
 
@@ -38,26 +44,41 @@ var printOrder = function (zombieName, zombEmail, gooeyness) {
     
     var nameListItem = document.createElement('li')
     nameListItem.textContent = zombieName
-    nameList.appendChild(nameListItem)
+    nameListContainer.appendChild(nameListItem)
     
     var emailListItem = document.createElement('li')
     emailListItem.textContent = zombEmail
-    emailList.appendChild(emailListItem)
+    emailListContainer.appendChild(emailListItem)
 
     var gooeynessListItem = document.createElement('li')
     gooeynessListItem.textContent = gooeyness
-    gooeynessList.appendChild(gooeynessListItem)
+    gooeynessListContainer.appendChild(gooeynessListItem)
 
     var brainServingImageJello = document.createElement('img')
     brainServingImageJello.src = "http://cdn.shopify.com/s/files/1/1365/2497/products/zombie_brain_gelatin_mold_2_grande.jpg?v=1520534078"
     brainServingImageJello.classList.add('brainsImages')
-    emailList.appendChild(brainServingImageJello)
+    emailListContainer.appendChild(brainServingImageJello)
 
     var brainServingImageFrapp = document.createElement('img')
     brainServingImageFrapp.src = "https://globalassets.starbucks.com/assets/d4dec9baeb6d49f893851ae0e921f6fa.jpg"
     brainServingImageFrapp.classList.add('brainsImages')
-    emailList.appendChild(brainServingImageFrapp)
+    emailListContainer.appendChild(brainServingImageFrapp)
 }
 
 orderForm.addEventListener('submit', submitAndReceiveBrains);
+
+$.ajax('https://dc-coffeerun.herokuapp.com/api/coffeeorders', {method: 'GET'})
+.then(function(data) {
+    for (var key in data) {
+        addNewOrderToAJAX(data[key])
+    }
+})
+
+
+var addNewOrderToAJAX = function (order) {
+    var newOrder = document.createElement('div');
+        newOrder.textContent = order._id
+        var orderList = document.querySelector('.order-list')
+        orderList.appendChild(newOrder)
+}
 
